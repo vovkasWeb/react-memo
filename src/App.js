148555eef54ef7133bdb24bca1dd0fd5fe8b6a0b
vovkasWeb <vova77557@gmail.com>
2023-10-25 20:32:58 +0300

    work with memo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, memo } from 'react'
+import { Container } from 'react-bootstrap'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function propsCompare(prevProps,nextProps){
+return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
 }
 
-export default App;
+const Form = memo(props => {
+	return (
+		<Container>
+			<form className='w-50 border mt-5 p-3 m-auto'>
+				<div className='mb-3'>
+					<label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
+						Email address
+					</label>
+					<input
+						value={props.mail}
+						type='email'
+						className='form-control'
+						id='exampleFormControlInput1'
+						placeholder='name@example.com'
+					/>
+				</div>
+				<div className='mb-3'>
+					<label htmlFor='exampleFormControlTextarea1' className='form-label'>
+						Example textarea
+					</label>
+					<textarea
+						value={props.text}
+						className='form-control'
+						id='exampleFormControlTextarea1'
+						rows='3'
+					></textarea>
+				</div>
+			</form>
+		</Container>
+	)
+},propsCompare)
+
+function App() {
+	const [data, setData] = useState({
+		mail: 'name@example.com',
+		text: 'some text',
+	})
+
+	return (
+		<>
+			<Form mail={data.mail} text={data.text} />
+			<button
+				onClick={() =>
+					setData({
+						mail: 'second@example.com',
+						text: 'another text',
+					})
+				}
+			>
+				Click me
+			</button>
+		</>
+	)
+}
+
+export default App
